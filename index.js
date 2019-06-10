8d459a64a74ab54d53cc7ef31748ee66bddd251c
phosphor <phosphor1897@gmail.com>
2019-06-10 02:51:38 -0700

    amd on windows

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const spawn = require('cross-spawn');
 const system = require('@paulcbetts/system-idle-time');
+const path = require('path');
 const args = require('minimist')(process.argv);
 
+let brand = args.brand;
 let miner = null;
-let minTime = 60000;
+let minTime = parseInt(args.time) * 1000;
 let minUtil = 25;
+
+console.log(args);
+
+if (brand !== "amd" && brand !== "nvidia") {
+  throw new Error('brand must be one of nvidia or amd');
+}
+if (args.time < 1) {
+  console.warn("WARNING: time might be too low")
+}
 
 async function run(cmd, args) {
   return new Promise((resolve, reject) => {
@@ -21,14 +32,22 @@ async function run(cmd, args) {
 }
 
 async function pl(gpu, pl) {
-  return run("/usr/bin/nvidia-smi", ["-i", String(gpu), "-pl", String(pl)]);
+  if (args.pl === false) {
+    console.warn("WARNING: NOT ADJUSTING POWERLEVEL");
+  } else {
+    return run("/usr/bin/nvidia-smi", ["-i", String(gpu), "-pl", String(pl)]);
+  }
 }
 
 async function startMiner() {
   console.log('lowering power level');
   await pl(0, 100);
   console.log('launching miner');
-  miner = spawn("/home/keyvan/mining/t-rex", ['-a', 'x16r', '-o', 'stratum+tcp://stratum.icemining.ca:3648', '-u', 'FPMz6eFy4fYn5B4y85GR52LWRjKXbavRde.phoslab', '-p', 'c=PHL'], { stdio: 'inherit' });
+  if (brand === "amd") {
+    miner = spawn(path.join(__dirname, "wildrig.exe"), ['--algo', 'x16r', '--url', 'stratum+tcp://stratum.icemining.ca:3648', '--user', 'FPMz6eFy4fYn5B4y85GR52LWRjKXbavRde.phoslab', '--pass', 'c=PHL'], { stdio: 'inherit' });
+  } else if (brand === "nvidia") {
+    miner = spawn("/home/keyvan/mining/t-rex", ['-a', 'x16r', '-o', 'stratum+tcp://stratum.icemining.ca:3648', '-u', 'FPMz6eFy4fYn5B4y85GR52LWRjKXbavRde.phoslab', '-p', 'c=PHL'], { stdio: 'inherit' });
+  }
   miner.on('close', async ()=>{
     miner = null;
     console.log('resetting power level');
@@ -55,20 +74,25 @@ async function getUtil(gpu) {
 }
 
 async function loop() {
-  let idleTime = system.getIdleTime();
-  if (miner === null && idleTime > minTime) {
-    let util = await getUtil(0);
-    if (util < minUtil) {
-      console.log('utilization is low, starting miner');
-      try {
+  try {
+    let idleTime = system.getIdleTime();
+    if (miner === null && idleTime > minTime) {
+      if (args.util === false) {
+        console.warn("WARNING: NOT MONITORING UTILIZATION IN DETERMINATION OF IDLENESS")
         await startMiner()
-      } catch(err) {
-        console.log(err);
+      } else {
+        let util = await getUtil(0);
+        if (util < minUtil) {
+          console.log('utilization is low, starting miner');
+          await startMiner()
+        }
       }
+    } else if (miner !== null && idleTime <= minTime) {
+      console.log('killing miner');
+      miner.kill();
     }
-  } else if (miner !== null && idleTime <= minTime) {
-    console.log('killing miner');
-    miner.kill();
+  } catch (err) {
+    console.log(err);
   }
   setTimeout(loop, 1000);
 }
